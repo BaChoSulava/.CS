bab09a54144a1559f0dde6e9d622d22d7d70cf3e
Bachana Sulava <117168936+BaChoSulava@users.noreply.github.com>
2025-11-06 20:54:01 +0400

    Refactor input loop for repeated execution
    Refactor the main program loop to allow for repeated input of rows, columns, and range limits. Adjust variable declarations and output statements for clarity.

--- DavDva/20251106.cs
+++ DavDva/20251106.cs
@@ -7,119 +7,131 @@ namespace _20251106
     {
         static void Main(string[] args)
         {
-            Console.Write("number of Rows: ");
-            int row = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("number of Colooms: ");
-            int colomn = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("numbers min range: ");
-            int limitDown = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("numbers Max range: ");
-            int limitUp = Convert.ToInt32(Console.ReadLine());
-
-            int[,] x = new int[row,colomn];
-            int sum = 0;
-            int max = limitDown;
-            int min = limitUp;
-            int? evenMax = null;
-            int? evenMin = null;
-            bool evenExists = false;
-            int evenSum = 0;
-            int? oddMax = null;
-            int? oddMin = null;
-            bool oddExists = false;
-            int oddSum = 0;
-
-            Console.WriteLine();
-            for (int i = 0; i < x.GetLength(0); i++)
+            string start = "y";
+            while (start == "y")
             {
-                for (int j = 0; j < x.GetLength(1); j++)
-                {
-                    x[i, j] = Random.Shared.Next(limitDown, limitUp);
-                    Console.Write($"{x[i, j]} ");
+                Console.Write("number of Rows: ");
+                int row = Convert.ToInt32(Console.ReadLine());
 
-                    sum += x[i, j];
+                Console.Write("number of Colooms: ");
+                int colomn = Convert.ToInt32(Console.ReadLine());
 
-                    if (max < x[i, j]) max = x[i, j];
-                    if (min > x[i, j]) min = x[i, j];
+                Console.Write("numbers min range: ");
+                int limitDown = Convert.ToInt32(Console.ReadLine());
 
-                    if (x[i,j] % 2 == 0)
-                    {
-                        evenExists = true;
-                        evenSum += x[i, j];
-                        if (evenMax == null || x[i, j] > evenMax) evenMax = x[i, j];
-                        if (evenMin == null || x[i, j] < evenMin) evenMin = x[i, j];
-                    }
-                    else
-                    {
-                        oddExists = true;
-                        oddSum += x[i, j];
-                        if (oddMax == null || x[i, j] > oddMax) oddMax = x[i, j];
-                        if (oddMin == null || x[i, j] < oddMin) oddMin = x[i, j];
-                    }
-                }
+                Console.Write("numbers Max range: ");
+                int limitUp = Convert.ToInt32(Console.ReadLine());
+
+                int[,] x = new int[row, colomn];
+                int sum = 0;
+                int max = limitDown;
+                int min = limitUp;
+                int? evenMax = null;
+                int? evenMin = null;
+                bool evenExists = false;
+                int evenSum = 0;
+                int? oddMax = null;
+                int? oddMin = null;
+                bool oddExists = false;
+                int oddSum = 0;
+
                 Console.WriteLine();
-            }
-
-            Console.WriteLine();
-
-            Console.Write("Sum: ");
-            Console.WriteLine(sum);
-
-            Console.Write("Avrg: ");
-            Console.WriteLine(sum / (x.GetLength(0) * x.GetLength(1)));
-
-            Console.Write("Max: ");
-            Console.WriteLine(max);
-
-            Console.Write("Min: ");
-            Console.WriteLine(min);
-
-            Console.Write("Even Sum: ");
-            Console.WriteLine(evenSum);
-
-            Console.Write("Odd Sum: ");
-            Console.WriteLine(oddSum);
-
-
-            if (evenExists)
-            {
-                if (evenMax.HasValue)
+                for (int i = 0; i < x.GetLength(0); i++)
                 {
-                    Console.Write("Even Max: ");
-                    Console.WriteLine(evenMax);
-                }
-                if (evenMin.HasValue)
-                {
-                    Console.Write("Even Min: ");
-                    Console.WriteLine(evenMin);
-                }
-            }
-            else
-            {
+                    for (int j = 0; j < x.GetLength(1); j++)
+                    {
+                        x[i, j] = Random.Shared.Next(limitDown, limitUp);
+                        Console.Write($"{x[i, j]} ");
 
-                Console.Write("there is no Even");
-            }
+                        sum += x[i, j];
 
-            if (oddExists)
-            {
-                if (oddMax.HasValue)
-                {
-                    Console.Write("Odd Max: ");
-                    Console.WriteLine(oddMax);
-                }
-                if (oddMin.HasValue)
-                {
-                    Console.Write("odd Min: ");
-                    Console.WriteLine(oddMin);
-                }
-            }
-            else
-            {
+                        if (max < x[i, j]) max = x[i, j];
+                        if (min > x[i, j]) min = x[i, j];
 
-                Console.Write("there is no Odd");
+                        if (x[i, j] % 2 == 0)
+                        {
+                            evenExists = true;
+                            evenSum += x[i, j];
+                            if (evenMax == null || x[i, j] > evenMax) evenMax = x[i, j];
+                            if (evenMin == null || x[i, j] < evenMin) evenMin = x[i, j];
+                        }
+                        else
+                        {
+                            oddExists = true;
+                            oddSum += x[i, j];
+                            if (oddMax == null || x[i, j] > oddMax) oddMax = x[i, j];
+                            if (oddMin == null || x[i, j] < oddMin) oddMin = x[i, j];
+                        }
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+
+                Console.Write("Sum: ");
+                Console.WriteLine(sum);
+
+                Console.Write("Avrg: ");
+                Console.WriteLine(sum / (x.GetLength(0) * x.GetLength(1)));
+
+                Console.Write("Max: ");
+                Console.WriteLine(max);
+
+                Console.Write("Min: ");
+                Console.WriteLine(min);
+
+                Console.Write("Even Sum: ");
+                Console.WriteLine(evenSum);
+
+                Console.Write("Odd Sum: ");
+                Console.WriteLine(oddSum);
+
+
+                if (evenExists)
+                {
+                    if (evenMax.HasValue)
+                    {
+                        Console.Write("Even Max: ");
+                        Console.WriteLine(evenMax);
+                    }
+                    if (evenMin.HasValue)
+                    {
+                        Console.Write("Even Min: ");
+                        Console.WriteLine(evenMin);
+                    }
+                }
+                else
+                {
+
+                    Console.Write("there is no Even");
+                }
+
+                if (oddExists)
+                {
+                    if (oddMax.HasValue)
+                    {
+                        Console.Write("Odd Max: ");
+                        Console.WriteLine(oddMax);
+                    }
+                    if (oddMin.HasValue)
+                    {
+                        Console.Write("odd Min: ");
+                        Console.WriteLine(oddMin);
+                    }
+                }
+                else
+                {
+
+                    Console.Write("there is no Odd");
+                }
+
+                Console.WriteLine(); 
+                Console.WriteLine();
+                Console.Write("to restart type 'y': ");
+                start = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("-------------------");
+                Console.WriteLine();
             }
         }
     }
