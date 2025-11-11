6049827cabdabc2d0066fd8adad1c9af04ac18f9
Bachana Sulava <117168936+BaChoSulava@users.noreply.github.com>
2025-11-11 08:39:30 +0400

    Update 20251109_unic_array.cs

--- DavDva/20251109_unic_array.cs
+++ DavDva/20251109_unic_array.cs
@@ -7,17 +7,21 @@ namespace _20251109
             int[,] x = new int[10, 8];
 
             // TODO: Fill the array with random unique numbers
-            for (int i = 0; i < x.GetLength(0); i++)
+            for (int row = 0; row < x.GetLength(0); row++)
             {
-                for (int j = 0; j < x.GetLength(1); j++)
+                for (int colomn = 0; colomn < x.GetLength(1); colomn++)
                 {
-                    x[i, j] = Random.Shared.Next(10, 100);
-                    for (int k = 0; k < j; k++)
+                    x[row, colomn] = Random.Shared.Next(10, 100);
+
+                    for (int i = 0; i < row; i++)
                     {
-                        if (x[k,j] == x[i,j])
+                        for (int j = 0; j < colomn; j++)
                         {
-                            k--;
-                            break;
+                            if (x[i, j] == x[row, colomn])
+                            {
+                                j--;
+                                break;
+                            }
                         }
                     }
                 }
