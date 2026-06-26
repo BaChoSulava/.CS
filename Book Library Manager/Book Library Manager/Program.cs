6ed60d35cbd058c8eb7b5e890010232f7d83929c
Bachana Sulava <117168936+BaChoSulava@users.noreply.github.com>
2026-06-26 11:48:17 +0400

    Add files via upload

---
+++ Book Library Manager/Book Library Manager/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BookLibraryManager
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+
+        
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        public Book(string title, double price)
+        {
+            _ = Title;
+            _ = Price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Price} GEL";
+        }
+    }
+
+    public static class LibraryHelper
+    {
+
+    }
+}
